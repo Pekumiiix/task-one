191ce0d978d84e5e9041331118516e7fd3460009
Pelumi Amao <113313095+Pekumiiix@users.noreply.github.com>
2023-09-11 13:53:14 +0100

    Initial Commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const currentDay = document.getElementById('day')
+const currentTime = document.getElementById('time')
+
+function getTime() {
+    const now = new Date()
+
+    const secondsTime = now.getSeconds()
+    const minutesTime = now.getMinutes()
+    const hourTime = now.getHours()
+    const milliSeconds = now.getMilliseconds()
+    
+    currentTime.innerHTML ='Time: ' + hourTime + 'hr(s)' + ' ' + minutesTime + 'min(s)' + ' ' + secondsTime + 'sec(s)' + ' ' + milliSeconds + "ms"
+}
+
+function getDay() {
+    const d = new Date()
+
+    const dayTime = d.getDay()
+
+    if (dayTime == 1) {
+        currentDay.innerHTML = 'Day: Monday'
+    } else if (dayTime == 2) {
+        currentDay.innerHTML = 'Day: Tuesday'
+    } else if (dayTime == 3) {
+        currentDay.innerHTML = 'Day: Wednesday'
+    } else if (dayTime == 4) {
+        currentDay.innerHTML = 'Day: Thursday'
+    } else if (dayTime == 5) {
+        currentDay.innerHTML = 'Day: Friday'
+    } else if (dayTime == 6) {
+        currentDay.innerHTML = 'Day: Saturday'
+    } else if (dayTime == 0) {
+        currentDay.innerHTML = 'Day: Sunday'
+    }
+}
+//getDay()
+setInterval(getDay, 100)
+setInterval(getTime, 10)
